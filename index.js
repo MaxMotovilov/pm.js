d92273a91bfb576ff33fe27e1b139278cf1417c2
Max <max@motovilov.com>
2020-05-13 13:05:34 -0500

    Naive implementation, sans parser

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const parseHeading = require('./parse');
+
+exports.firstOf = matchPattern(firstMatch);
+exports.distinct = matchPattern(onlyMatch);
+
+const wrapper = Symbol('pm.js: compiled wrapper');
+
+function matchPattern(strategy) {
+    return (template, ...functions) =>
+        (...args) => functions[
+            (template[wrapper] || (
+                template[wrapper] = strategy(functions.map(fn => parseHeading(fn.toString())))
+            ))(args)
+        ](...args);
+}
+
+function firstMatch(patterns) {
+    return args => assertValid(
+        patterns.findIndex(pattern => pattern(args)),
+        index => index>=0,
+        "Arguments don't match any of the patterns"
+    );
+}
+
+function onlyMatch(patterns) {
+    const first = firstMatch(patterns);
+    return args => assertValid(
+        first(args),
+        index => !patterns.slice(index).some(pattern => pattern(args)),
+        "Arguments match multiple patterns"
+    );
+}
+
+function assertValid(value, isValid, message) {
+    if(!isValid(value))
+        throw TypeError(message);
+    else
+        return value;
+}
+
