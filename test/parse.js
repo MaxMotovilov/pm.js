f6b334092d5267bbe587b10b1a4e1b9ef8ff573b
Max <max@motovilov.com>
2020-05-18 16:52:13 -0500

    Use tape & faucet to run the tests

--- test/parse.js
+++ test/parse.js
@@ -1,44 +1,64 @@
 "use strict";
 
-const {parseHeading} = require('../parse');
+const
+    test = require('tape-catch'),
+    {parseHeading} = require('../parse');
 
-function test(fn) {
-    try {
-        parseHeading(fn.toString());
-    } catch(err) {
-        console.log(err);
-        console.log(`in ${fn.toString()}`);
+test('parseHeading() [parse.js]', ({ok, notOk, end}) => {
+    [
+        function() {}
+    ,
+        function(x) {}
+    ,
+        function foo() {}
+    ,
+        () => {}
+    ,
+        x => x
+    ,
+        (x=1) => {}
+    ,
+        ([,a,b]) => {}, yes([]), no({}), no(),
+    ,
+        ([a,,b]) => {}, yes([]), no({}), no(),
+    ,
+        ([a=(c+d),,b]) => {}, yes([]), no({}), no(),
+    ,
+        ([a,[b]]) => {}, yes([,[]]), no([]), no({}), no(),
+    ,
+        ([a,[b]=[1]]) => {}, yes([]), no({}), no(),
+    ,
+        ({a, b}) => {}, yes({}), yes([]), no(),
+    ,
+        ({a=1, b:{c}}) => {}, yes({b:{}}), yes({b:[]}), no({}), no([]), no(),
+    ,
+        ({1:a, "-":b=2}) => {}, yes({}), yes([]), no(),
+    ,
+        ({a, [0xff]:[{b}]}) => {}, yes({255:[{}]}), yes({255:[[]]}), no({255:{}}), no({}), no()
+    ].reduce(
+        (last, what) => (what.isPredicate ? (what(last), last) : parse(what)), null
+    );
+    end();
+
+    function parse(source) {
+        const result = parseHeading(source.toString());
+        result.source = source;
+        return result;
     }
-}
 
-[
-    function() {}
-,
-    function(x) {}
-,
-    function foo() {}
-,
-    () => {}
-,
-    x => x
-,
-    (x=1) => {}
-,
-    ([,a,b]) => {}
-,
-    ([a,,b]) => {}
-,
-    ([a=(c+d),,b]) => {}
-,
-    ([a,[b]]) => {}
-,
-    ([a,[b]=[1]]) => {}
-,
-    ({a, b}) => {}
-,
-    ({a=1, b:{c}}) => {}
-,
-    ({1:a, "-":b=2}) => {}
-,
-    ({a, [0xff]:[{b}]}) => {}
-].forEach(test);
+    function yes(...testValues) {
+        const result = patternMatcher => ok(patternMatcher(testValues), `${format(testValues)} matches ${patternMatcher.source}`);
+        result.isPredicate = true;
+        return result;
+    }
+
+    function no(...testValues) {
+        const result = patternMatcher => notOk(patternMatcher(testValues), `${format(testValues)} does not match ${patternMatcher.source}`);
+        result.isPredicate = true;
+        return result;
+    }
+});
+
+function format(testValues) {
+    return `(${testValues.map(v => JSON.stringify(v)).join(', ')})`;
+}
