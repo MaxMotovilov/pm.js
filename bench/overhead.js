1ac2c1de5954bff535e7ba1d02cdf4e70b9eebd5
Max <max@motovilov.com>
2020-05-20 13:23:09 -0500

    Benchmarking against (...args) =>
    --
    Raw: ({prop}) => prop: 125ms per 100M
    Baseline: ({prop}) => prop: 212ms per 10M +1595%
    Wrapped: ({prop}) => prop: 537ms per 10M +153% +4196%
    Raw: ({prop1}, {prop2}) => prop1+prop2: 757ms per 100M
    Baseline: ({prop1}, {prop2}) => prop1+prop2: 189ms per 10M +149%
    Wrapped: ({prop1}, {prop2}) => prop1+prop2: 522ms per 10M +176% +589%

--- bench/overhead.js
+++ bench/overhead.js
@@ -8,9 +8,12 @@ const
 
 function test(what, arg1, arg2) {
     const wrapped = firstOf`${what}`;
+    const unpacked = (...args) => what(...args);
 
-    const base = time(what, arg1, arg2, 100000000, `Raw: ${what.toString()}`);
-    time(wrapped, arg1, arg2, 10000000, `Wrapped: ${what.toString()}`, base);
+    const
+        raw = time(what, arg1, arg2, 100000000, `Raw: ${what.toString()}`),
+        baseline = time(unpacked, arg1, arg2, 10000000, `Baseline: ${what.toString()}`, raw);
+    time(wrapped, arg1, arg2, 10000000, `Wrapped: ${what.toString()}`, baseline, raw);
 }
 
 test(getProp, {prop:1});
@@ -20,13 +23,13 @@ function shorten(n) {
     return n.toString().replace(/000$/, 'K').replace(/000K$/, 'M');
 }
 
-function time(what, arg1, arg2, count, title, base) {
+function time(what, arg1, arg2, count, title, ...bases) {
     const start = Date.now();
     for(let i=0; i<count; ++i)
         what(arg1, arg2);
     const t = Date.now() - start;
 
-    console.log(`${title}: ${t}ms per ${shorten(count)}`, base ? `+${Math.floor(100*(t/1000/count-base)/base)}%` : '');
+    console.log(`${title}: ${t}ms per ${shorten(count)}`, ...bases.map(base => `+${Math.floor(100*(t/1000/count-base)/base)}%`));
 
     return t/1000/count;
 }
